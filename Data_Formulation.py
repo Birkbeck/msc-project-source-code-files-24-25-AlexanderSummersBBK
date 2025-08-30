148537d5492fd8f27dd3c8cb2574c0fd0a6de90b
AJSUMMERS100 <alex.summers42@icloud.com>
2025-08-30 12:00:42 +0100

    Shortened the names of the CSV files.

---
+++ Data_Formulation.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import pandas as pd
+
+
+path = Path.cwd()
+paths = {"path_hackmageddon": path / "Hackmageddon.csv", 
+         "path_dates": path / "Dates.csv", 
+         "path_ISO": path / "ISO.csv", 
+         "path_wcci": path / "WCCI.csv"}
+
