ed37ecf22e3820aa0e6cd7d1a07ea15936787619
AJSUMMERS100 <alex.summers42@icloud.com>
2025-09-04 20:51:42 +0100

    Combined the dates_id and calendar dataset together as well as adding a number of attacks on that given day.

--- Explainably_Forecasting_Cyber_Threats.py
+++ Explainably_Forecasting_Cyber_Threats.py
@@ -2,6 +2,8 @@ import pandas as pd
 from pathlib import Path
 from sklearn.model_selection import train_test_split
 from datetime import datetime, timedelta
+import numpy as np
+
 
 # Defines the path to the data
 path = Path.cwd()
@@ -18,7 +20,22 @@ for i in range(len(dates)-1):
 start, end = datetime.strptime("01/01/2016", "%d/%m/%Y"), datetime.strptime("28/02/2025", "%d/%m/%Y")
 calendar = {}
 while start <= end:
-    calendar[start.strftime("%d/%m/%Y")] = []
+    calendar[start.strftime("%d/%m/%Y")] = [[]]
     start += timedelta(days=1)
-print(calendar)
+
+# Combines dates_id and calendar by cycling through the dates in calendar.
+# Once a date in calendar has a coressponding array of IDs, each of the IDs 
+# will be implemented into the arrays within the calendar.
+data_without_dates = data.drop(columns=["ID", "Date"])
+for each in calendar:
+    if each in dates_id:
+        for thy in dates_id[each]:
+            calendar[each][0].append(data_without_dates[data["ID"] == thy])
+
+# Adding a number of attacks to each date
+for each in calendar:
+    calendar[each].append(len(calendar[each][0]))
+
+
+print(calendar["01/01/2016"])
 
