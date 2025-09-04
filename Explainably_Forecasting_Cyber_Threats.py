2599d5c2bd5a394f65c01a43391d64d8a0c9393b
AJSUMMERS100 <alex.summers42@icloud.com>
2025-09-04 20:00:50 +0100

    Added a calendar that stores all dates between 01/01/2016 and 28/02/2025. The aim is to combine the current data with this calendar to show any lull periods of no attacks.

--- Explainably_Forecasting_Cyber_Threats.py
+++ Explainably_Forecasting_Cyber_Threats.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from pathlib import Path
 from sklearn.model_selection import train_test_split
+from datetime import datetime, timedelta
+
 
 path = Path.cwd()
 data = pd.DataFrame(pd.read_csv(path / "main_table.csv"))
@@ -10,4 +12,9 @@ dates_id = {}
 for i in range(len(dates)-1):
     if dates[i] not in dates_id:
         dates_id[dates[i]] = data[data["Date"] == dates[i]]["ID"].tolist()
-print(dates_id)
+
+start, end = datetime.strptime("01/01/2016", "%d/%m/%Y"), datetime.strptime("28/02/2025", "%d/%m/%Y")
+calendar = []
+while start <= end:
+    calendar.append(start.strftime("%d/%m/%Y"))
+    start += timedelta(days=1)
