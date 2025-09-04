bdc66547e8178d54a5b57534cd27ab22bff12c4b
AJSUMMERS100 <alex.summers42@icloud.com>
2025-09-04 13:46:46 +0100

    Have now added a library that stores all the ids of the rows according to the date of attack.

--- Explainably_Forecasting_Cyber_Threats.py
+++ Explainably_Forecasting_Cyber_Threats.py
@@ -1,7 +1,13 @@
 import pandas as pd
 from pathlib import Path
-
+from sklearn.model_selection import train_test_split
 
 path = Path.cwd()
-data = pd.DataFrame(path / "main_table.csv")
+data = pd.DataFrame(pd.read_csv(path / "main_table.csv"))
 
+dates = data["Date"]
+dates_id = {}
+for i in range(len(dates)-1):
+    if dates[i] not in dates_id:
+        dates_id[dates[i]] = data[data["Date"] == dates[i]]["ID"].tolist()
+print(dates_id)
