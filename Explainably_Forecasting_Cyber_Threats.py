0545a6e5c9d8ac92e2e1098f955a313b569c9104
AJSUMMERS100 <alex.summers42@icloud.com>
2025-09-04 20:04:14 +0100

    Updated the calendar so that it is stored as a library, rather than an array.

--- Explainably_Forecasting_Cyber_Threats.py
+++ Explainably_Forecasting_Cyber_Threats.py
@@ -3,18 +3,22 @@ from pathlib import Path
 from sklearn.model_selection import train_test_split
 from datetime import datetime, timedelta
 
-
+# Defines the path to the data
 path = Path.cwd()
 data = pd.DataFrame(pd.read_csv(path / "main_table.csv"))
 
+# Creates a library of dates with associated IDs, that are stored in an array
 dates = data["Date"]
 dates_id = {}
 for i in range(len(dates)-1):
     if dates[i] not in dates_id:
         dates_id[dates[i]] = data[data["Date"] == dates[i]]["ID"].tolist()
 
+# Creates a calendar within a library
 start, end = datetime.strptime("01/01/2016", "%d/%m/%Y"), datetime.strptime("28/02/2025", "%d/%m/%Y")
-calendar = []
+calendar = {}
 while start <= end:
-    calendar.append(start.strftime("%d/%m/%Y"))
+    calendar[start.strftime("%d/%m/%Y")] = []
     start += timedelta(days=1)
+print(calendar)
+
